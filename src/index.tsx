77d1b80345e5cc287b089408a274d5d9ae3b1fd9
Róbert Kjaran <robert@kjaran.com>
2023-03-15 17:15:31 +0000

    Remove Thing

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,4 @@
-import React, { FC, HTMLAttributes } from 'react';
 import ConnectedChat, { ConnectedChatProps } from './views/ConnectedChat';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
 
 export {
   ConnectedChat,
