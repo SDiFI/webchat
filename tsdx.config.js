bb62677e6633c5bbb18f5547c9ca2dba1a41a297
Róbert Kjaran <robert@kjaran.com>
2023-03-31 08:33:06 +0000

    Add polyfills for node builtins required for UMD build
    The nice-grpc-web package needs node builtin polyfills to work in a standalone
    UMD build.

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,8 @@
 const images = require('@rollup/plugin-image');
 const commonjs = require('@rollup/plugin-commonjs');
 const replace = require('@rollup/plugin-replace');
+const nodeResolve = require('@rollup/plugin-node-resolve').default;
+const nodePolyfill = require('rollup-plugin-polyfill-node');
 
 const react = require('react');
 const reactDom = require('react-dom');
@@ -12,18 +14,33 @@ module.exports = {
         if (config.output.format === 'umd') {
             delete config.external;
 
-            config.plugins = config.plugins.map(p =>
-                p.name === 'commonjs'
-                ? commonjs({
-                    namedExports: {
-                        'react': Object.keys(react),
-                        'react-dom': Object.keys(reactDom),
-                        'react-is': Object.keys(reactIs),
-                        'styled-components': Object.keys(styled),
-                    },
-                })
-                : p
-            );
+            config.plugins = config.plugins.map(p => {
+                if (p.name === 'commonjs') {
+                    return commonjs({
+                        namedExports: {
+                            'react': Object.keys(react),
+                            'react-dom': Object.keys(reactDom),
+                            'react-is': Object.keys(reactIs),
+                            'styled-components': Object.keys(styled),
+                            'nice-grpc-web': ['createChannel', 'createClient'],
+                        },
+                    });
+                } else if (p.name === 'node-resolve') {
+                    return nodeResolve({
+                        browser: true,
+                        preferBuiltins: false,
+                    });
+                }
+
+                return p
+            });
+
+            config.plugins = [
+                ...config.plugins.slice(0, 1),
+                nodePolyfill({
+                    include: ["buffer", "stream", "events"],
+                }), ...config.plugins.slice(1)];
+
         }
 
         config.plugins = config.plugins.map(p =>
