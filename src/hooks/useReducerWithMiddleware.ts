cbc487e96b22f5509e951f197913789caa342bb8
Róbert Kjaran <robert@kjaran.com>
2023-04-27 16:51:42 +0000

    Add a reducer hook that supports {middle,after}ware for side effects on actions

---
+++ src/hooks/useReducerWithMiddleware.ts
@@ -0,0 +1,29 @@
+import React, { useEffect, useReducer, useRef } from 'react';
+
+/**
+ * Reducer hook with multiple middleware functions and multiple afterware functions for side-effects.
+ */
+export function useReducerWithMiddleware<S, A>(
+    reducer: React.Reducer<S, A>,
+    initialState: S,
+    middlewareFns: Array<(action: A, state: S) => void>,
+    afterwareFns: Array<(action: A, state: S) => void>): [S, React.Dispatch<A>] {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const actionRef = useRef<A>();
+
+    const dispatchWithMiddleware: React.Dispatch<A> = (action: A) => {
+        middlewareFns.forEach((fn) => fn(action, state));
+        actionRef.current = action;
+        dispatch(action);
+    };
+
+    useEffect(() => {
+        if (!actionRef.current)
+            return;
+        afterwareFns.forEach((fn) => actionRef.current && fn(actionRef.current, state));
+        actionRef.current = undefined;
+
+    }, [afterwareFns, state]);
+
+    return [state, dispatchWithMiddleware];
+}
