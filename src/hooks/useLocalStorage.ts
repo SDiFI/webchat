419677dda879f4ff084c491a072af287d0c0e988
Róbert Kjaran <robert@kjaran.com>
2023-05-03 12:28:40 +0000

    Refactor use{Local,Session}Storage hooks to share an implementation

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,21 +1,4 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import useStorage from './useStorage';
 
-function getLocalStorageOrDefault<T>(key: string, defaultValue: T): T {
-    const stored = localStorage.getItem(key);
-    if (!stored) {
-        return defaultValue;
-    }
-    return JSON.parse(stored) as T;
-}
-
-export default function useLocalStorage<T>(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>] {
-    const [value, setValue] = useState<T>(
-        getLocalStorageOrDefault(key, defaultValue)
-    );
-
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
-
-    return [value, setValue];
-}
+const useLocalStorage = <T,>(key: string, defaultValue: T) => useStorage(localStorage, key, defaultValue);
+export default useLocalStorage;
